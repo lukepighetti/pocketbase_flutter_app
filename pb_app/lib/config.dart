5452e726c1e9461688a23974d0297740f5bc7b8c
david <ds@dalsegno-solutions.com>
2022-09-20 21:38:19 +0200

    Make the environment variable names what the README.md says they are

--- pb_app/lib/config.dart
+++ pb_app/lib/config.dart
@@ -6,6 +6,6 @@ class Config {
 
   static const skipLogin = true;
 
-  static const testEmail = String.fromEnvironment('testEmail');
-  static const testPassword = String.fromEnvironment('testPassword');
+  static const testEmail = String.fromEnvironment('PBF_TEST_EMAIL');
+  static const testPassword = String.fromEnvironment('PBF_TEST_PASSWORD');
 }
