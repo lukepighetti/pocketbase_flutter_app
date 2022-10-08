a963db5384d0a119397ff9f64b11de750957e545
corralforum <corralforum@gmail.com>
2022-10-08 12:53:39 -0700

    Move submissions out of the widget

--- pb_app/lib/models/submission_form_state.dart
+++ pb_app/lib/models/submission_form_state.dart
@@ -4,6 +4,11 @@ import 'package:flutter/material.dart' show Colors;
 import 'package:flutter/widgets.dart';
 import 'package:palette_generator/palette_generator.dart';
 
+enum ScreenshotSubmissionLocations {
+  home,
+  lock,
+}
+
 class SubmissionFormState extends ChangeNotifier {
   Color? _card1Dominant;
   Color _selectedBackgroundColor = Colors.white;
@@ -13,9 +18,34 @@ class SubmissionFormState extends ChangeNotifier {
   bool get isLightBackground =>
       _selectedBackgroundColor.computeLuminance() > 0.5;
 
+  File? _homescreenImage;
+  File? _lockscreenImage;
+
+  File? get homeScreenImage => _homescreenImage;
+  File? get lockscreenImage => _lockscreenImage;
+
   SubmissionFormState();
 
-  void setCard1Image(File image) async {
+  void setSelectedCardImage(
+    File image,
+    ScreenshotSubmissionLocations screen,
+  ) {
+    if (screen == ScreenshotSubmissionLocations.home) {
+      _homescreenImage = image;
+    } else if (screen == ScreenshotSubmissionLocations.lock) {
+      _lockscreenImage = image;
+    }
+
+    notifyListeners();
+  }
+
+  void setCardImage(
+    File image,
+    ScreenshotSubmissionLocations screen,
+  ) async {
+    //Set the selected card background first, generating a palette has a delay
+    setSelectedCardImage(image, screen);
+
     final palette = await PaletteGenerator.fromImageProvider(FileImage(image));
     _card1Dominant = palette.dominantColor?.color;
     final Color pastelTone;
